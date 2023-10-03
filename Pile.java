f446d56088349d61970a60a232548ba6b4133b21
AsteriLykos <105269399+AsteriLykos@users.noreply.github.com>
2023-10-03 12:59:52 +0000

    début classe Pile

--- Pile.java
+++ Pile.java
@@ -2,10 +2,22 @@ public class Pile {
 
   private List   lstCalcul ;
   
-  public Pile (  ){
+  public Pile ()
+  {
+    lstCalcul = new ArrayList<String>() ;    
+  }
 
-    lstCalcul = new ArrayList<char>() ; 
+  public String depiler()
+  {
+    if( lstCalcul.size() == 0 )
+      return null;
     
+    return lstCalcul.get(0);
+  }
+
+  public void empiler( String s )
+  {
+    lstCalcul.add(s);
   }
   
 }
